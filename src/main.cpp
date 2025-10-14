d3693a64f0ecb50f8dba2b871a35f9758e950d4d
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2025-10-14 08:44:47 -0400

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,90 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Wed Sep 25 2019                                           */
+/*    Description:  Clawbot Template (Individual Motors)                      */
+/*                                                                            */
+/*    Name:                                                                   */
+/*    Date:                                                                   */
+/*    Class:                                                                  */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// LeftMotor            motor         1               
+// RightMotor           motor         10              
+// ClawMotor            motor         3               
+// ArmMotor             motor         8               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+// Include the V5 Library
+#include "vex.h"
+  
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+void  dispaly()
+{
+  //init but not main screeen
+  //made to look like linux boot screnn
+  //this linux boot screen is based off my hp envy
+  Brain.Screen.print("mounting /home");
+  Brain.Screen.newLine();
+  Brain.Screen.print("[ok] mounted /home");  
+  Brain.Screen.newLine();
+  Brain.Screen.print("[ok] reached target for local filesystems");
+  Brain.Screen.newLine();
+  Brain.Screen.print("[ok] reached target for local filesystems");
+  Brain.Screen.newLine();
+  Brain.Screen.print("[ok] reached target for local filesystems");
+  Brain.Screen.newLine();
+
+
+
+  //Brain.Screen.clearScreen();
+  Brain.Screen.print("VEXcode");
+
+  //Brain.Screen.clearScreen();
+  Brain.Screen.print("VEXcode");
+
+}
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  printf("project started");
+  // Begin project code
+  //calling stuff
+  dispaly();
+}
+
+
+
+void mainui()
+{
+  
+}
+
+void battery()
+{
+Brain.Battery.voltage(volt);
+Brain.Battery.current(amp);
+Brain.Battery.current();
+}
+
+void moterinfo()
+{
+//motor 3
+Motor3.position(degrees);
+
+}
+
+void Controller()
+{
+//call this after main it sequnce with display
+Controller1.Screen.print("VEXcode");
+
+}
